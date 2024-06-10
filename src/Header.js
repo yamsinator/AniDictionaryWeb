517ad1b228088e5794cf2ffd1026105e57872b9c
yamsinator <BuiSteven@gmail.com>
2024-06-10 14:04:29 -0700

    Fixed Header href to bring user back to the proper homescreen

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ import './Header.css'
 const Header = () => {
     return (
         <header className="app-header">
-           <a href="/home" style={{ textDecoration: 'none', color: 'inherit' }}>
+           <a href="/" style={{ textDecoration: 'none', color: 'inherit' }}>
         <h1>AniDictionary</h1>
       </a>
         </header>
